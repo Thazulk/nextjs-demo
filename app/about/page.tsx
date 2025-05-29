37a417400d5080d5592c1086b952a770efd98802
Thazulk <thazulk@gmail.com>
2025-05-29 15:29:37 +0200

    feat: add About and Contact pages with basic content

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page of our application.</p>
+      <p>We are committed to providing the best service possible.</p>
+    </div>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>This is the content page of our application.</p>
+      <p>Here you can find various articles and resources.</p>
+    </div>
+  );
+}
