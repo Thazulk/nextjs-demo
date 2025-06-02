5da82b08903f698ac92367ce3c3db67517d58924
Thazulk <thazulk@gmail.com>
2025-06-02 16:03:23 +0200

    refactor: remove console log for API response in Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,6 @@ export default async function Contact() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await response.json();
 
-  console.log("Response from API:", posts);
   return (
     <div>
       <h1>Contact Page</h1>
